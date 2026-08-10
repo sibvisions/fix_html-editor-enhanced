feafd32b5f78b41ff5ffa4f4827f4b752804373a
rjahn <rene.jahn@sibvisions.com>
2026-08-10 12:43:36 +0200

    dependencies

--- example/ios/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
+++ example/ios/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
@@ -15,20 +15,20 @@ let package = Package(
         .library(name: "FlutterGeneratedPluginSwiftPackage", type: .static, targets: ["FlutterGeneratedPluginSwiftPackage"])
     ],
     dependencies: [
-        .package(name: "zikzak_inappwebview_ios", path: "../.packages/zikzak_inappwebview_ios-4.3.3"),
-        .package(name: "pointer_interceptor_ios", path: "../.packages/pointer_interceptor_ios-0.10.1+1"),
+        .package(name: "file_picker", path: "../.packages/file_picker-11.0.3"),
         .package(name: "flutter_keyboard_visibility", path: "../.packages/flutter_keyboard_visibility"),
-        .package(name: "file_picker", path: "../.packages/file_picker-10.3.10"),
+        .package(name: "pointer_interceptor_ios", path: "../.packages/pointer_interceptor_ios-0.10.1+1"),
+        .package(name: "zikzak_inappwebview_ios", path: "../.packages/zikzak_inappwebview_ios-4.7.0"),
         .package(name: "FlutterFramework", path: "../.packages/FlutterFramework")
     ],
     targets: [
         .target(
             name: "FlutterGeneratedPluginSwiftPackage",
             dependencies: [
-                .product(name: "zikzak-inappwebview-ios", package: "zikzak_inappwebview_ios"),
-                .product(name: "pointer-interceptor-ios", package: "pointer_interceptor_ios"),
-                .product(name: "flutter-keyboard-visibility", package: "flutter_keyboard_visibility"),
                 .product(name: "file-picker", package: "file_picker"),
+                .product(name: "flutter-keyboard-visibility", package: "flutter_keyboard_visibility"),
+                .product(name: "pointer-interceptor-ios", package: "pointer_interceptor_ios"),
+                .product(name: "zikzak-inappwebview-ios", package: "zikzak_inappwebview_ios"),
                 .product(name: "FlutterFramework", package: "FlutterFramework")
             ]
         )
